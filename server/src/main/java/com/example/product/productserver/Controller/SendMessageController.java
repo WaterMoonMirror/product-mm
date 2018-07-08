19e49272cfd6c86c4bfda98956787749773b2a78
tkn <lizhu0227@163.com>
2018-07-08 22:30:27 +0800

    rabbitmq 接收消息的三种方式

---
+++ server/src/main/java/com/example/product/productserver/Controller/SendMessageController.java
@@ -0,0 +1,28 @@
+package com.example.product.productserver.Controller;
+
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+/**
+ * @Auther: tkn
+ * @Date: 2018/7/8 22:02
+ * @Description:
+ */
+@RestController
+public class SendMessageController {
+    @Autowired
+    private AmqpTemplate amqpTemplate;
+
+    /**
+     *
+     * 发送消息
+     */
+    @GetMapping("/sendMessage")
+    public void sendMessage(){
+            amqpTemplate.convertAndSend("MyQueue","now "+new Date());
+    }
+}
---
+++ server/src/main/java/com/example/product/productserver/message/MqReceiver.java
@@ -0,0 +1,49 @@
+package com.example.product.productserver.message;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.*;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Auther: tkn
+ * @Date: 2018/7/8 21:59
+ * @Description:
+ */
+@Component
+@Slf4j
+public class MqReceiver {
+    /**
+     * 第一种创建方式：需要自己添加队列
+     *  接受消息
+     * @param message
+     */
+//    @RabbitListener(queues="MyQueue")
+//    public void process(String message){
+//        log.info("MqReceiver:{}",message);
+//
+//    }
+
+    /**
+     * 第二种创建方式：自动添加队列
+     * @param message
+     */
+//    @RabbitListener(queuesToDeclare=@Queue("MyQueue"))
+//    public void process(String message){
+//        log.info("MqReceiver:{}",message);
+//
+//    }
+
+    /**
+     * 第三种创建方式：自动添加队列,并且队列和Exchange绑定
+     * @param message
+     */
+    @RabbitListener(bindings =@QueueBinding(
+            value = @Queue("MyQueue"),
+            exchange = @Exchange("MyExchange")
+    ))
+    public void process(String message){
+        log.info("MqReceiver:{}",message);
+
+    }
+
+}
