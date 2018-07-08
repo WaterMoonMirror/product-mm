5eadfda80e15a615a0ac8d1d527792c46f8049cc
tkn <lizhu0227@163.com>
2018-07-08 17:58:02 +0800

    添加统一配置中心

--- server/src/main/java/com/example/product/productserver/Controller/ServerController.java
+++ server/src/main/java/com/example/product/productserver/Controller/ServerController.java
@@ -2,11 +2,17 @@ package com.example.product.productserver.Controller;
 
 import com.example.product.productserver.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class ServerController {
+
+    @Value("${env}")
+    private String env;
 
     @Autowired
     private ProductService productService;
@@ -14,6 +20,11 @@ public class ServerController {
     @GetMapping("/msg")
     public String msg() {
         return "this is product' msg";
+    }
+
+    @GetMapping("/getEnv")
+    public String getEnv(){
+        return env;
     }
 
 
